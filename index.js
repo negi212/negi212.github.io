25407110a830cda0efdae6a2bf438de45e9a48ef
negi212 <186390248+negi212@users.noreply.github.com>
2024-10-26 21:52:01 +0900

    renew files

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const BUTTON_CLICK_EVENT= document.getElementById('test1');
+const BUTTON_CLICK_EVENT= document.getElementById('submit');
+const setNum = document.getElementById("setnumber");
+const selectNum = document.getElementById("number");
 BUTTON_CLICK_EVENT.addEventListener('click', () => {
-    alert("hello,world")
+    const num = selectNum.selectedIndex;
+    // 選択した要素のテキストを取得する処理
+    const getNum = selectNum.options[num].innerText;
+    // 取得した要素をHTMLに埋め込む
+    setNum.innerText = getNum;
 });
