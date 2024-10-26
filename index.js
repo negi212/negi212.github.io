6aa736be15ec9fe802bc1d834089ff4ed028cd14
negi212 <186390248+negi212@users.noreply.github.com>
2024-10-26 22:10:29 +0900

    renew

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const BUTTON_CLICK_EVENT= document.getElementById('submit');
-const setNum = document.getElementById("setnumber");
-const selectNum = document.getElementById("number");
-BUTTON_CLICK_EVENT.addEventListener('click', () => {
-    const num = selectNum.selectedIndex;
-    // 選択した要素のテキストを取得する処理
-    const getNum = selectNum.options[num].innerText;
-    // 取得した要素をHTMLに埋め込む
-    setNum.innerText = getNum;
-});
+function butotnClick(){
+    document.getElementById('selected').innerText = '選択されているのは ' + numSelect.value + ' です';
+  }
+  
+  let numSelect = document.getElementById('number');
+  let text = document.getElementById('selected').innerText;
+  numSelect.options[0].selected = true;
+  
+  let checkButton = document.getElementById('checkButton');
+  checkButton.addEventListener('click', butotnClick);
